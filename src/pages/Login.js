257a8ebe6d64cc00cea6b82246ede6af7cdb6e17
minseong0213 <minseong0213@naver.com>
2023-07-23 00:17:36 +0900

    kakaologin

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 const Login=(props)=>{
+
+    const Rest_api_key='c684aed1126dd79ff99c6f8e0964d4fa' //REST API KEY
+    const redirect_uri = 'http://localhost:8000/kakao/' //Redirect URI
+    // oauth 요청 URL
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
+    const handleLogin = ()=>{
+        window.location.href = kakaoURL
+    }
     
     return(
         <div>
                 <Link to="/SignUp" style={{textDecoration:'none'}}>
                     <button>회원가입</button>
                 </Link>
+                <button onClick={handleLogin}>카카오 로그인</button>
         </div>
     );
     
 };
 
-export default Login;
+
+export default Login
+
+
